d2cfa1f059a59c57b4b8f93bbbe7a1927913ff2e
pgm-lukagrav <lukagrav@student.arteveldehs.be>
2025-08-21 19:37:43 +0200

    fixed main.jsx imports

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-// Import the generated route tree
-import { routeTree } from './routeTree.gen'
+const queryClient = new QueryClient();
 
-// Create a new router instance
-const router = createRouter({ routeTree })
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-   <RouterProvider router={router} />
-  </StrictMode>,
-)
+const router = createRouter({ routeTree });
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
+
