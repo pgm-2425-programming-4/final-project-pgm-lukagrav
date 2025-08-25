9d46c587ea4ffe9236d0f68ff260bebbe368cf9c
pgm-lukagrav <lukagrav@student.arteveldehs.be>
2025-08-25 13:46:34 +0200

    Added css for TaskCloumn and card using Bulma and linked css file

--- client/src/components/TaskCard/TaskCard.jsx
+++ client/src/components/TaskCard/TaskCard.jsx
@@ -1,11 +1,17 @@
+import React from 'react';
+import './TaskCard.css';
+
 const TaskCard = ({ task, onClick }) => {
   return (
-    <div onClick={onClick}>
-      <h4>{task.title}</h4>
+    <div className="task-card box" onClick={onClick}>
+      <h4 className="task-card-title">{task.title}</h4>
+
       {task.labels?.length > 0 && (
-        <ul>
+        <ul className="task-card-labels">
           {task.labels.map((label) => (
-            <li key={label.id}>{label.title}</li>
+            <li key={label.id} className="task-card-label">
+              {label.title}
+            </li>
           ))}
         </ul>
       )}
--- client/src/components/TaskColumn/Taskcolumn.jsx
+++ client/src/components/TaskColumn/Taskcolumn.jsx
@@ -1,20 +1,32 @@
 import TaskCard from "../TaskCard/TaskCard";
+import "./TaskColumn.css"
 
 const TaskColumn = ({ state, tasks, onTaskClick }) => {
   const tasksForState = tasks.filter((task) => task.state === state);
 
   return (
-    <div>
-      <h3>{state}</h3>
-      {tasksForState.length > 0 ? (
-        tasksForState.map((task) => (
-          <TaskCard key={task.id} task={task} onClick={() => onTaskClick(task)} />
-        ))
-      ) : (
-        <p>No tasks</p>
-      )}
+    <div className="column is-one-quarter">
+      <div className="box task-column-box">
+        <h3 className="title is-5 has-text-centered mb-4">{state}</h3>
+  
+        {tasksForState.length > 0 ? (
+          <div className="content">
+            {tasksForState.map((task) => (
+              <TaskCard
+                key={task.id}
+                task={task}
+                onClick={() => onTaskClick(task)}
+              />
+            ))}
+          </div>
+        ) : (
+          <p className="has-text-grey-light has-text-centered">No tasks</p>
+        )}
+      </div>
     </div>
   );
+  
+  
 };
 
 export default TaskColumn;
