59ed6045997afb30e416128d5a9b884ea51c3bb2
pgm-lukagrav <lukagrav@student.arteveldehs.be>
2025-08-25 13:39:24 +0200

    Added css for AppLayout: navigation and child routes

--- client/src/components/AppLayout/AppLayout.jsx
+++ client/src/components/AppLayout/AppLayout.jsx
@@ -1,13 +1,20 @@
 // src/components/AppLayout/AppLayout.jsx
 import { Outlet } from "@tanstack/react-router";
 import Sidebar from "../Sidebar/Sidebar";
+import "./AppLayout.css"
 
 export default function AppLayout() {
   return (
-    <div>
-      <Sidebar />
+    <div className="container is-fluid">
+      <div className="columns">
+        <div className="column is-one-quarter">
+          <Sidebar />
+        </div>
 
-      <Outlet />
+        <div className="column main-content">
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 }
