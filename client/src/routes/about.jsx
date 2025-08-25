2c19866b84594aa906979eb56a930019f0c4c164
pgm-lukagrav <lukagrav@student.arteveldehs.be>
2025-08-25 21:44:07 +0200

    final commit: Changed content on about page

--- client/src/routes/about.jsx
+++ client/src/routes/about.jsx
@@ -10,11 +10,14 @@ function About() {
       <div className="container content">
         <h1 className="title is-2">About This Project</h1>
         <p>
-          This project is a task management board built with React and Strapi.
-          It allows users to organize tasks by state, associate them with
-          projects, and categorize them using labels. The application was
-          developed to practice frontend and backend integration, RESTful APIs,
-          and user interface design.
+          This project is a task management board built with React and Strapi,
+          styled using Bulma for a modern and responsive design. It allows users
+          to organize tasks by state, associate them with projects, and
+          categorize them using labels. The application demonstrates integration
+          between frontend and backend using RESTful APIs. There is a version
+          that runs locally for development and testing, as well as a live
+          version with the frontend deployed on Netlify and the backend hosted
+          on Render.
         </p>
 
         <h2 className="title is-4 mt-5">Contact</h2>
